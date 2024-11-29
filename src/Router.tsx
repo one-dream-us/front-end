e23477977ab5672694d4835615fd9dbdb32ab2c0
DxxxHxx <donghun1163@gmail.com>
2024-11-29 13:48:52 +0900

    feat: 레이아웃 컴포넌트 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import DashBoard from "./pages/DashBoard";
+import { Routes, Route } from 'react-router-dom';
+import DashBoard from './pages/DashBoard';
+import Login from './pages/Login';
+import Layout from './components/common/Layout';
 
 const AppRoutes = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/" element={<DashBoard />} />
+      <Route element={<Layout />}>
+        <Route path='/' element={<DashBoard />} />
+        <Route path='/login' element={<Login />} />
+      </Route>
     </Routes>
   );
 };
