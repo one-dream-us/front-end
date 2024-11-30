ff70d12cdcb2085b48175da019eaacc7a95132e6
DxxxHxx <donghun1163@gmail.com>
2024-11-30 18:58:25 +0900

    chore: msw 설치,세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '@/assets/fonts/font.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const enableMocking = async () => {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+  return worker.start();
+};
+
+(async () => {
+  try {
+    await enableMocking();
+    createRoot(document.getElementById('root')!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    );
+  } catch (e) {
+    console.log(e);
+  }
+})();
