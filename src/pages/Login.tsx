5a6ac72024b0ee5a43ac000b9795d97c21e31955
DxxxHxx <donghun1163@gmail.com>
2024-12-03 19:13:57 +0900

    refactor: 로그인 페이지 ui 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,13 +3,15 @@ import logo from '@/assets/imgs/main_logo.svg';
 
 export default function Login() {
   return (
-    <div className='flex items-center justify-center h-full'>
-      <div className='m-auto w-[343px] md:w-[368px] lg:w-[488px] px-4'>
-        <div className='w-full h-14  lg:h-[72px] m-auto mb-44 md:mb-60 lg:mb-56 text-4xl font-extrabold flex justify-center items-center'>
-          <img src={logo} className='w-[72px] h-14' alt='' />
-          <h1>이게머니</h1>
+    <div className='absolute left-0 top-0 flex h-screen w-full'>
+      <div className='flex w-full flex-1 items-center justify-center bg-white'>
+        <div className='m-auto w-[343px] px-4 md:w-[368px] lg:w-[488px]'>
+          <div className='m-auto mb-44 flex h-14 w-full items-center justify-center text-4xl font-extrabold md:mb-60 lg:mb-56 lg:h-[72px]'>
+            <img src={logo} className='h-14 w-[72px]' alt='' />
+            <h1>이게머니</h1>
+          </div>
+          <KakaoLoginButton />
         </div>
-        <KakaoLoginButton />
       </div>
     </div>
   );
