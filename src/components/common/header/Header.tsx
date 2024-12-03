1fa203ae4a84fd3311f65d92026416599c3455ec
DxxxHxx <donghun1163@gmail.com>
2024-12-03 19:14:52 +0900

    refactor: header ui 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -19,17 +19,17 @@ export default function Header() {
   }
 
   return (
-    <header className='fixed left-0 top-0 z-[999] flex h-20 w-full items-center justify-between bg-white px-5 py-12 md:px-20 lg:px-32'>
+    <header className='fixed left-0 top-0 z-[999] flex h-[52px] w-full items-center justify-between bg-white px-4 text-sm md:px-6 md:py-[11px] lg:h-20 lg:px-32 lg:py-6'>
       <div className='flex items-center justify-start'>
         <Link
-          className='mr-20 flex items-center justify-center gap-x-1 text-xl font-extrabold'
+          className='mr-10 flex items-center justify-center gap-x-1 text-xl font-extrabold'
           to={'/'}
         >
           <img src={logo} alt='' />
           <h1 className='hidden md:block'>이게머니</h1>
         </Link>
 
-        <ul className='hidden items-center justify-center gap-5 text-custom-gray md:flex md:gap-10 lg:gap-x-16'>
+        <ul className='hidden items-center justify-center text-custom-gray md:flex md:gap-8 lg:gap-x-16'>
           {HeaderMenuList.map((item) => (
             <li key={item.id}>
               <Link className={pathname === item.to ? 'font-bold text-black' : ''} to={item.to}>
@@ -40,9 +40,25 @@ export default function Header() {
         </ul>
       </div>
 
-      <Link className='hidden md:block' to={data ? '' : '/login'}>
-        <button>{isLoading ? 'loading...' : data ? 'my page' : 'login'}</button>
-      </Link>
+      {data ? (
+        <>
+          {/* desktop */}
+          <div className={`hidden h-9 w-9 rounded-full bg-custom-gray lg:block`}></div>
+          {/* tablet */}
+          <div
+            className={`hidden h-11 w-11 items-center justify-center bg-custom-gray-light md:flex lg:hidden`}
+          >
+            <div className='h-6 w-6 bg-custom-gray'></div>
+          </div>
+        </>
+      ) : (
+        <Link
+          className={`hidden h-[30px] w-[96px] items-center justify-center rounded-xl bg-custom-gray-medium py-2 md:flex ${isLoading && 'animate-pulse'}`}
+          to={'/login'}
+        >
+          <button>{isLoading ? '' : data ? 'my page' : 'login'}</button>
+        </Link>
+      )}
 
       <button onClick={handleShowSlider} className='z-[999] block md:hidden'>
         <svg
