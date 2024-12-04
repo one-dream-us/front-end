c6183f1bd1e43d61c7b6f0fceb62e46097300cd5
DxxxHxx <donghun1163@gmail.com>
2024-12-04 19:11:18 +0900

    hotfix: axios instance baseURL 수정

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_SERVER_URL,
+  baseURL: `${import.meta.env.VITE_BACKEND_SERVER_URL}api/v1`,
   withCredentials: true,
 });
 
