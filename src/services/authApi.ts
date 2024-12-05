0fa61f2925b474b0330cf9b77b77be3f3e551c43
DxxxHxx <donghun1163@gmail.com>
2024-12-05 21:10:43 +0900

    fix: api endpoint 변경

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -1,10 +1,10 @@
 import client from '@/utils/client';
 
 const authApi = {
-  getInfo: async () => (await client.get('/user/info')).data,
+  getInfo: async () => (await client.get('/users/info')).data,
   logout: async () => {
     try {
-      await client.post('/user/logout');
+      await client.post('/users/logout');
       location.href = '/';
     } catch (e) {
       console.log(e);
@@ -12,7 +12,7 @@ const authApi = {
   },
   withDraw: async () => {
     try {
-      await client.delete('/user/withdraw');
+      await client.delete('/users/withdraw');
       location.href = '/withdraw-success';
     } catch {
       alert('다시 시도해주세요');
