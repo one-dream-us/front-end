a885f19b717e2ec6078eb067f93efac65dbcc047
choycoy <passionfruits479@gmail.com>
2024-12-05 12:54:53 +0900

    fix: temp

--- src/components/contentDetail/ScriptList.tsx
+++ src/components/contentDetail/ScriptList.tsx
@@ -1,24 +1,31 @@
 import ScriptWithTime from '@/components/contentDetail/ScriptWithTime';
 import { handleScriptClick } from '@/handlers/contentDetail/handleScriptClick';
+import { ScriptParagraph } from '@/types/interface';
+import { convertTimeToSeconds } from '@/utils/contentDetailUtils';
 
 export default function ScriptList({
   fullText,
   setVideoTime,
 }: {
-  fullText: { time: string; text: string }[];
+  fullText: ScriptParagraph[];
   setVideoTime: (time: number) => void;
 }) {
   return (
     <div className='md:mb-10 desktop:mb-[37px]'>
-      {fullText.map((item, index) => (
-        <ScriptWithTime
-          key={index}
-          id={`script-${index}`}
-          time={item.time}
-          script={item.text}
-          onClick={() => handleScriptClick(item.time, index, setVideoTime)}
-        />
-      ))}
+      {fullText.map((item, index) => {
+        const timeInSeconds = convertTimeToSeconds(item.timestamp);
+
+        return (
+          <ScriptWithTime
+            key={item.id}
+            id={`script-${index}`}
+            time={timeInSeconds}
+            script={item.paragraphText}
+            onClick={() => handleScriptClick(timeInSeconds, index, setVideoTime)}
+            dictionaries={item.dictionaries}
+          />
+        );
+      })}
     </div>
   );
 }
