598dba1a5b2217cbcd52a21d899310ddd235452b
DxxxHxx <donghun1163@gmail.com>
2024-12-05 20:05:04 +0900

    [DEV-62] design: contents 컴포넌트 작성, home page에 추가

--- src/components/common/contentCard/KeywordTags.tsx
+++ src/components/common/contentCard/KeywordTags.tsx
@@ -3,7 +3,7 @@ export default function KeywordTags({ tags }: { tags: string[] }) {
     <div className='flex h-5 w-full gap-x-1 text-[9px]'>
       {tags.map((tag, index) => (
         <span key={index} className='h-5 whitespace-nowrap rounded-[4px] border p-1 leading-[10px]'>
-          {tag}
+          #{tag}
         </span>
       ))}
     </div>
