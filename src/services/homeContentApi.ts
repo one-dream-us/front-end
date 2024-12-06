1b143325a98dc9962eb82be66b7bf41f53376417
choycoy <passionfruits479@gmail.com>
2024-12-06 20:55:02 +0900

    fix: 변경된 api 주소값 적용

--- src/services/homeContentApi.ts
+++ src/services/homeContentApi.ts
@@ -1,8 +1,8 @@
 import client from '@/utils/client';
 
 const homeContentApi = {
-  getPopular: async () => (await client.get('/content/popular')).data,
-  getLatest: async () => (await client.get('/content/latest')).data,
+  getPopular: async () => (await client.get('/contents/popular')).data,
+  getLatest: async () => (await client.get('/contents/latest')).data,
 };
 
 export default homeContentApi;
