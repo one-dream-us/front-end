1aa3f7845146e847ea024fe9a764d681e1c4b9af
choycoy <passionfruits479@gmail.com>
2024-12-06 21:00:30 +0900

    fix: 변경된 api 경로 적용

--- src/services/contentAPi.ts
+++ src/services/contentAPi.ts
@@ -2,7 +2,7 @@ import client from '@/utils/client';
 
 const contentApi = {
   getContentDetail: async (contentId: number) => {
-    const { data: response } = await client.get(`/content/${contentId}`);
+    const { data: response } = await client.get(`/contents/${contentId}`);
     console.log('조회성공');
     return response;
   },
