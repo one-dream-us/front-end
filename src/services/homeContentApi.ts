92077e92433f2f5f29a13c8c4d8c49f0543dc530
DxxxHxx <donghun1163@gmail.com>
2024-12-06 18:48:19 +0900

    fix: api 주소 변경

--- src/services/homeContentApi.ts
+++ src/services/homeContentApi.ts
@@ -3,14 +3,14 @@ import client from '@/utils/client';
 const homeContentApi = {
   getPopular: async () => {
     try {
-      return (await client.get('/content/popular')).data;
+      return (await client.get('/contents/popular')).data;
     } catch (e) {
       console.log(e);
     }
   },
   getLatest: async () => {
     try {
-      return (await client.get('/content/latest')).data;
+      return (await client.get('/contents/latest')).data;
     } catch (e) {
       console.log(e);
     }
