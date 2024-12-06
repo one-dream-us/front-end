c9656a178278025a3fc9f398206c91f062694b0d
choycoy <passionfruits479@gmail.com>
2024-12-06 12:32:58 +0900

    fix: api 경로 롤백
    Temp

--- src/services/scrapApi.ts
+++ src/services/scrapApi.ts
@@ -2,34 +2,32 @@ import client from '@/utils/client';
 
 const scrapApi = {
   getScrapedContents: async () => {
-    const { data: response } = await client.get('/scraps/contents');
+    const { data: response } = await client.get('/scrap/content');
     return response;
   },
 
   getScrapedTerms: async () => {
-    const { data: response } = await client.get('/scraps/dictionaries');
+    const { data: response } = await client.get('/scrap/dictionary');
     return response;
   },
 
   deleteScrapedContent: async (contentId: number) => {
-    const { data: response } = await client.delete(`/scraps/contents/${contentId}`);
-
+    const { data: response } = await client.delete(`/scrap/content/${contentId}`);
     return response;
   },
 
   deleteScrapedTerm: async (termId: number) => {
-    const { data: response } = await client.delete(`/scraps/dictionaries/${termId}`);
-
+    const { data: response } = await client.delete(`/scrap/dictionary/${termId}`);
     return response;
   },
 
   addScrapContent: async (contentId: number) => {
-    const { data: response } = await client.post(`/scraps/contenst/${contentId}`);
+    const { data: response } = await client.post(`/scrap/content/${contentId}`);
     return response;
   },
 
   addScrapTerm: async (termId: number) => {
-    const { data: response } = await client.post(`/scraps/dictionaries/${termId}`);
+    const { data: response } = await client.post(`/scrap/dictionary/${termId}`);
     return response;
   },
 };
