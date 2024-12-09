adf7e23582868cba0b20a6b835cb2f5308191965
DxxxHxx <donghun1163@gmail.com>
2024-12-10 02:08:32 +0900

    refactor: 조회수 반영

--- src/hooks/homeContent/useMyViewCount.ts
+++ src/hooks/homeContent/useMyViewCount.ts
@@ -5,9 +5,12 @@ import { useLoginStore } from '@/store/useIsLoginStore';
 export const useMyViewCount = () => {
   const isLogin = useLoginStore((state) => state.isLogin);
 
+  console.log(isLogin);
+
   return useQuery({
     queryKey: ['myViewCount'],
     queryFn: contentApi.getMyViewCount,
     enabled: isLogin,
+    staleTime: 0,
   });
 };
