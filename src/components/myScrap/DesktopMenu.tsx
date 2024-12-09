140c3c0944cbbdd3aeed7b63270359bb88978b6e
choycoy <passionfruits479@gmail.com>
2024-12-10 03:14:30 +0900

    design: 마이스크랩 네비게이션바 스타일링 수정

--- src/components/myScrap/DesktopMenu.tsx
+++ src/components/myScrap/DesktopMenu.tsx
@@ -14,7 +14,7 @@ export default function DesktopMenu({
   const setAllIdList = useMyScrapStore((state) => state.setAllIdList);
 
   return (
-    <ul className='relative hidden h-[58px] w-[71px] flex-col gap-y-[17px] self-start before:absolute before:left-0 before:top-[3px] before:h-[54px] before:w-[1px] before:bg-custom-gray-500 md:text-sm desktop:flex'>
+    <ul className='relative hidden h-[58px] w-[71px] flex-col gap-y-[17px] self-start before:absolute before:left-0 before:top-[3px] before:h-[51px] before:w-[1px] before:bg-custom-gray-500 md:text-sm desktop:flex'>
       {MenuItems.map((item) => (
         <li
           key={item}
@@ -26,7 +26,7 @@ export default function DesktopMenu({
           }}
           className={`relative cursor-pointer whitespace-nowrap pl-3.5 font-medium md:text-sm ${
             activeMenu === item
-              ? `text-custom-gray-dark after:absolute after:-left-[1px] ${activeMenu === '콘텐츠' ? 'after:top-[1px]' : 'after:bottom-0'} after:h-3.5 after:w-[3px] after:rounded-full after:bg-custom-gray-dark`
+              ? `text-custom-gray-dark after:absolute after:-left-[1px] ${activeMenu === '콘텐츠' ? 'after:top-[3px]' : 'after:bottom-[3px]'} after:h-3.5 after:w-[3px] after:rounded-full after:bg-custom-gray-dark`
               : 'text-custom-gray-500'
           }`}
         >
