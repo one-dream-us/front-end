7d51520e7ecaa7d7f5eedf0c59cff87ad4773575
DxxxHxx <donghun1163@gmail.com>
2024-12-10 00:55:00 +0900

    design: 탈퇴 모달 간격 수정

--- src/components/withdraw/WithdrawModal.tsx
+++ src/components/withdraw/WithdrawModal.tsx
@@ -1,6 +1,4 @@
 import exit_png from '@/assets/this_is_money_imgs/img_png/exit.png';
-import exit_webp from '@/assets/this_is_money_imgs/img_webp/exit.webp';
-import ImgContainer from '../common/ImgContainer';
 import { useCloseModal } from '@/hooks/ui/useCloseModal';
 export default function WithdrawModal({
   closeModal,
@@ -14,19 +12,24 @@ export default function WithdrawModal({
     <div className='fixed left-0 top-0 z-[1000] flex h-screen w-full items-center justify-center bg-black bg-opacity-60 px-4'>
       <div className='relative flex h-[287px] w-[343px] flex-col items-center justify-start rounded-lg bg-white px-5 py-4 md:h-[347px] md:w-[372px]'>
         <div className='h-[205px] w-[175px] md:h-[265px] md:w-[322px]'>
-          <ImgContainer
+          {/* <ImgContainer
             imgs={{ png: exit_png, webp: exit_webp }}
-            className='m-auto mb-5 h-[120px] w-full md:mb-[28px] md:h-[145px] md:w-[166px]'
+            className='m-auto mb-5 h-[120px] w-full md:h-[180px] md:w-[166px]'
+          /> */}
+          <img
+            className='mb-5 h-[120px] w-full md:h-[180px] md:max-w-[332px]'
+            src={exit_png}
+            alt=''
           />
           <div className='h-[65px] w-full text-center'>
-            <h1 className='text-sm font-bold'>정말 탈퇴하시겠어요?</h1>
-            <span className='text-xs text-custom-gray-dark'>
+            <h1 className='mb-3 text-sm font-bold text-custom-gray-dark'>정말 탈퇴하시겠어요?</h1>
+            <span className='text-xs leading-[150%] text-custom-gray-dark'>
               탈퇴하면 계정 정보가 영구 삭제되고,
               <br /> 저장된 스크랩도 다시 볼 수 없어요!
             </span>
           </div>
         </div>
-        <div className='mt-auto flex h-[30px] w-[303px] items-center justify-between md:mb-auto'>
+        <div className='mt-auto flex h-[30px] w-[303px] items-center justify-between md:mt-auto'>
           <button
             onClick={closeModal}
             className='h-[30px] w-[147px] rounded-[4px] bg-custom-gray-300 text-xs text-custom-gray-600 hover:bg-custom-gray-light-h'
