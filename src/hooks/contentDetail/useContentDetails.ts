85d7dceb29b35ca63acbd0bc618702f69ee0a4c1
choycoy <passionfruits479@gmail.com>
2024-12-10 01:53:15 +0900

    refactor: camelCase 파일명 수정 (contentAPi -> contentApi)

--- src/hooks/contentDetail/useContentDetails.ts
+++ src/hooks/contentDetail/useContentDetails.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import contentApi from '@/services/contentAPi';
+import contentApi from '@/services/contentApi';
 
 export default function useContentDetails(contentId: number) {
   const {
