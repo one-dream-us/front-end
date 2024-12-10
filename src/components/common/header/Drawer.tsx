61fcc9c682888ea114e7e3aa766f49f1918747fb
DxxxHxx <donghun1163@gmail.com>
2024-12-10 22:51:12 +0900

    design: drawer 패딩 수정

--- src/components/common/header/Drawer.tsx
+++ src/components/common/header/Drawer.tsx
@@ -88,7 +88,7 @@ export default function Drawer({
           )}
         </div>
 
-        <ul className='absolute top-[262px] flex h-[139px] w-[200px] flex-col items-start justify-between px-8 text-[22px] font-medium text-custom-gray-medium'>
+        <ul className='absolute top-[262px] flex h-[139px] w-[200px] flex-col items-start justify-between text-[22px] font-medium text-custom-gray-medium'>
           {HeaderMenuList.map((item) => (
             <Link
               className={
