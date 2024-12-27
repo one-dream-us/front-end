1071d443cee0329d73f46034046b05baae000f17
choycoy <passionfruits479@gmail.com>
2024-12-28 00:14:50 +0900

    design: 체크박스 데스크톱에선 크기 키우기

--- src/components/myScrap/content/Thumbnail.tsx
+++ src/components/myScrap/content/Thumbnail.tsx
@@ -9,7 +9,7 @@ export default function Thumbnail({ src, alt, id }: { src: string; alt: string; 
     <div className='relative h-full w-[168px] md:w-[260px] desktop:w-[343px]'>
       {isEditing && (
         <div className='absolute left-[2px] top-[3px]'>
-          <Checkbox id={id} />
+          <Checkbox id={id} activeMenu='콘텐츠' />
         </div>
       )}
       <img src={src} alt={alt} className='aspect-video h-full w-full rounded-[4px]' />
