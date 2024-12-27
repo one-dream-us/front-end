783ede9edf3caaf6ff48772d3984beaeb4f5fb57
choycoy <passionfruits479@gmail.com>
2024-12-27 22:32:41 +0900

    design: footer 스타일링 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer className='flex h-[140px] w-full items-center justify-center bg-custom-gray-lighter px-4 text-custom-gray-dark md:px-6 desktop:h-[120px] desktop:px-0'>
-      <div className='flex h-[79px] w-full items-center justify-between text-[10px] md:h-[85px] md:items-end md:text-xs desktop:w-[1440px] desktop:px-[129px]'>
+      <div className='flex w-full items-center justify-between text-[10px] md:h-[85px] md:items-end md:text-xs desktop:w-[1440px] desktop:px-[129px]'>
         <div className='flex w-[165px] flex-col gap-y-2'>
           <img src={logoWithName} alt='이게 머니 로고' className='h-[56px] w-[76px]' />
           <a className='ml-1 cursor-pointer' href='mailto:thisismoney.241209@gmail.com'>
@@ -13,9 +13,9 @@ export default function Footer() {
           </a>
         </div>
         <nav>
-          <ul className='gap-y-2 underline md:flex md:gap-x-10 md:gap-y-0'>
+          <ul className='flex flex-col gap-y-3 underline md:flex-row md:gap-x-10 md:gap-y-0'>
             {FooterMenuList.map((item) => (
-              <li key={item.id}>
+              <li key={item.id} className='h-[11px] md:h-4'>
                 <Link to={item.to} target='_blank' rel='noopener noreferrer'>
                   {item.title}
                 </Link>
