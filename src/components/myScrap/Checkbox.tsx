d18353218f7e4187049784257ba0a51212cc6617
choycoy <passionfruits479@gmail.com>
2024-12-27 23:31:19 +0900

    design: 체크박스 데스크탑 사이즈 변경

--- src/components/myScrap/Checkbox.tsx
+++ src/components/myScrap/Checkbox.tsx
@@ -4,14 +4,14 @@ import { useSelectedIdList } from '@/hooks/myScrap/useSelectedIdList';
 export default function Checkbox({ id }: { id: number }) {
   const { selectedIdList, setSelectedIdList, allIdList, setIsAllChecked } = useSelectedIdList();
   return (
-    <label>
+    <label className='h-5 desktop:h-6'>
       <input
         type='checkbox'
         checked={selectedIdList.includes(id)}
         onChange={(e) =>
           handleConCheckChange(e, selectedIdList, id, setSelectedIdList, allIdList, setIsAllChecked)
         }
-        className='checkbox'
+        className='checkbox desktop:!h-6 desktop:!w-6'
       />
     </label>
   );
