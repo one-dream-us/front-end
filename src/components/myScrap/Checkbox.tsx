5cbd0f07d8415541e5bde53a83066ffc17cd5532
choycoy <passionfruits479@gmail.com>
2024-12-27 23:59:04 +0900

    Update Checkbox.tsx

--- src/components/myScrap/Checkbox.tsx
+++ src/components/myScrap/Checkbox.tsx
@@ -4,14 +4,14 @@ import { useSelectedIdList } from '@/hooks/myScrap/useSelectedIdList';
 export default function Checkbox({ id }: { id: number }) {
   const { selectedIdList, setSelectedIdList, allIdList, setIsAllChecked } = useSelectedIdList();
   return (
-    <label className='h-5 desktop:h-6'>
+    <label className='h-[18px]'>
       <input
         type='checkbox'
         checked={selectedIdList.includes(id)}
         onChange={(e) =>
           handleConCheckChange(e, selectedIdList, id, setSelectedIdList, allIdList, setIsAllChecked)
         }
-        className='checkbox desktop:!h-6 desktop:!w-6'
+        className='checkbox'
       />
     </label>
   );
