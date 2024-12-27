c58fcbcc86f1076936ea6aef7ab809322e52a3cc
choycoy <passionfruits479@gmail.com>
2024-12-27 20:13:28 +0900

    design: footer 폰트 사이즈 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,23 +5,18 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer className='flex h-[140px] w-full items-center justify-center bg-custom-gray-lighter px-4 text-custom-gray-dark md:px-6 desktop:h-[120px] desktop:px-0'>
-      <div className='flex h-[79px] w-full items-center justify-between md:h-[85px] md:items-end desktop:w-[1440px] desktop:px-[129px]'>
+      <div className='flex h-[79px] w-full items-center justify-between text-[10px] md:h-[85px] md:items-end md:text-xs desktop:w-[1440px] desktop:px-[129px]'>
         <div className='flex w-[165px] flex-col gap-y-2'>
           <img src={logoWithName} alt='이게 머니 로고' className='h-[56px] w-[76px]' />
-          <a className='ml-1 cursor-pointer text-[11px]' href='mailto:thisismoney.241209@gmail.com'>
+          <a className='ml-1 cursor-pointer' href='mailto:thisismoney.241209@gmail.com'>
             thisismoney.241209@gmail.com
           </a>
         </div>
         <nav>
-          <ul className='gap-y-2 md:flex md:gap-x-10 md:gap-y-0'>
+          <ul className='gap-y-2 underline md:flex md:gap-x-10 md:gap-y-0'>
             {FooterMenuList.map((item) => (
               <li key={item.id}>
-                <Link
-                  to={item.to}
-                  className='text-[11px] underline'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
+                <Link to={item.to} target='_blank' rel='noopener noreferrer'>
                   {item.title}
                 </Link>
               </li>
