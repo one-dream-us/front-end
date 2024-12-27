248978516b830c0ac4124c49e20c7972ce1f7b20
choycoy <passionfruits479@gmail.com>
2024-12-27 22:46:56 +0900

    fix: 체크박스에 label 추가

--- src/components/myScrap/Checkbox.tsx
+++ src/components/myScrap/Checkbox.tsx
@@ -4,13 +4,15 @@ import { useSelectedIdList } from '@/hooks/myScrap/useSelectedIdList';
 export default function Checkbox({ id }: { id: number }) {
   const { selectedIdList, setSelectedIdList, allIdList, setIsAllChecked } = useSelectedIdList();
   return (
-    <input
-      type='checkbox'
-      checked={selectedIdList.includes(id)}
-      onChange={(e) =>
-        handleConCheckChange(e, selectedIdList, id, setSelectedIdList, allIdList, setIsAllChecked)
-      }
-      className='checkbox'
-    />
+    <label>
+      <input
+        type='checkbox'
+        checked={selectedIdList.includes(id)}
+        onChange={(e) =>
+          handleConCheckChange(e, selectedIdList, id, setSelectedIdList, allIdList, setIsAllChecked)
+        }
+        className='checkbox'
+      />
+    </label>
   );
 }
