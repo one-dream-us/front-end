72a0e6c49448696808dcf655d78c7d5d6700eb1f
choycoy <passionfruits479@gmail.com>
2024-12-29 23:07:30 +0900

    refactor: 공유 모달 비지니스 로직 및 ui 로직 분리

--- src/components/contentDetail/ShareModal.tsx
+++ src/components/contentDetail/ShareModal.tsx
@@ -1,11 +1,6 @@
 import shareUtils from '@/handlers/contentDetail/handleShare';
 import closeGIcon from '@/assets/icons/X=Grey 30.svg';
-import useShareModal from '@/hooks/contentDetail/useShareModal';
-import copyLinkIcon from '@/assets/icons/copy-link.svg';
-import kakaoTalkIcon from '@/assets/imgs/카카오톡.jpg';
-import facebookIcon from '@/assets/imgs/페이스북.jpg';
-import XIcon from '@/assets/imgs/X 트위터.jpg';
-import useToastStore from '@/store/useToastStore';
+import useShareModalLogic from '@/hooks/contentDetail/logic/useShareModalLogic';
 
 export default function ShareModal({
   setIsShareModalOpen,
@@ -18,10 +13,8 @@ export default function ShareModal({
   description: string;
   img: string;
 }) {
-  const currentUrl = window.location.href;
-  useShareModal();
-  const showToast = useToastStore((state) => state.showToast);
-
+  const { currentUrl, showToast, copyLinkIcon, kakaoTalkIcon, facebookIcon, XIcon } =
+    useShareModalLogic();
   const buttonData = [
     {
       label: '링크 복사',
@@ -51,7 +44,7 @@ export default function ShareModal({
 
   return (
     <section className='absolute right-0 top-0 z-[50] flex w-[276px] flex-col gap-y-3 rounded-[10px] bg-modal px-5 py-4 text-custom-gray-lighter'>
-      <div className='flex items-center justify-between'>
+      <div className='flex justify-between items-center'>
         <h3 className='md:text-sm'>공유하기</h3>
         <button
           type='button'
@@ -82,14 +75,14 @@ export default function ShareModal({
             }
             type='button'
           >
-            <div className='flex flex-col items-center gap-y-1'>
-              <div className='group relative'>
+            <div className='flex flex-col gap-y-1 items-center'>
+              <div className='relative group'>
                 <img
                   src={imgSrc}
                   alt={alt}
                   className='h-[44px] w-[44px] overflow-hidden rounded-md'
                 />
-                <div className='absolute inset-0 overflow-hidden rounded-md bg-black opacity-0 transition md:group-hover:opacity-50' />
+                <div className='overflow-hidden absolute inset-0 bg-black rounded-md opacity-0 transition md:group-hover:opacity-50' />
               </div>
               <span className='whitespace-nowrap text-[10px]'>{label}</span>
             </div>
