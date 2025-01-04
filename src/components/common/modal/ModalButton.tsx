bdb015f54c39b5d67f141b3c1589ebd167b10508
DxxxHxx <donghun1163@gmail.com>
2025-01-04 23:41:41 +0900

    refactor: modal button 컴포넌트 props 리팩토링

--- src/components/common/modal/ModalButton.tsx
+++ src/components/common/modal/ModalButton.tsx
@@ -1,26 +1,28 @@
 export default function ModalButton({
   onClose,
-  onLogin,
+  onProceed,
   isChecked,
+  buttonText,
 }: {
   onClose: () => void;
-  onLogin: () => void;
+  onProceed: () => void;
   isChecked?: boolean;
+  buttonText: { close: string; proceed: string };
 }) {
   return (
     <div className='flex h-[44px] w-full gap-x-2 self-center transition'>
       <button
         onClick={onClose}
-        className='h-[44px] w-1/2 cursor-pointer whitespace-nowrap rounded bg-custom-gray-300 px-[59px] py-3 text-xs font-medium text-custom-gray-600 transition-all duration-200 hover:bg-hover-30'
+        className='h-[44px] w-1/2 cursor-pointer whitespace-nowrap rounded bg-custom-gray-300 px-[59px] py-3 text-xs font-medium text-custom-gray-dark transition-all duration-200 hover:bg-hover-30'
       >
-        닫기
+        {buttonText.close}
       </button>
 
       <button
-        onClick={onLogin}
+        onClick={onProceed}
         className={`h-[44px] w-1/2 whitespace-nowrap rounded py-3 text-xs font-medium transition-all duration-200 ${isChecked === false ? 'bg-custom-gray-600 text-custom-gray-300' : 'bg-custom-gray-dark text-custom-green-money hover:bg-hover-80 hover:text-green-hover'}`}
       >
-        로그인 하기
+        {buttonText.proceed}
       </button>
     </div>
   );
