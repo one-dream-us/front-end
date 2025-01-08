7c85c47430089bc79b898f18240e5647dd3329e2
DxxxHxx <donghun1163@gmail.com>
2025-01-08 17:48:03 +0900

    feat: 퀴즈 목록 fetch hook 작성

--- src/hooks/quiz/useQuizQuery.ts
+++ src/hooks/quiz/useQuizQuery.ts
@@ -2,15 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export interface IQuiz {
-  problem: string;
+  question: string;
   id: number;
   done: boolean;
   answer: string;
-  options: {
-    id: number;
-    option: string;
-    desc: string;
-  }[];
+  options: IOPtions[];
+}
+
+export interface IOPtions {
+  id: number;
+  option: string;
+  desc: string;
 }
 const useQuizQuery = () => {
   return useQuery<IQuiz[]>({
