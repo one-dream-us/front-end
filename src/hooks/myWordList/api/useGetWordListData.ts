6aea506e1a75d57956db253641501a430bafcddf
choycoy <passionfruits479@gmail.com>
2025-01-11 23:32:44 +0900

    feat: 핵심노트 추가, 삭제, 각 노트 리스트 호출, 학습 상태 api 바인딩

---
+++ src/hooks/myWordList/api/useGetWordListData.ts
@@ -0,0 +1,48 @@
+import { useLoginStore } from '@/store/useIsLoginStore';
+import wordListAPi from '@/services/wordListApi';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { MyWordListMenuType } from '@/types/types';
+import scrapApi from '@/services/scrapApi';
+
+export default function useGetWordListData(activeMenu: MyWordListMenuType) {
+  const navigate = useNavigate();
+  const { isLogin } = useLoginStore((state) => state);
+
+  const apiFunctions: Record<MyWordListMenuType, () => Promise<any>> = {
+    스크랩: scrapApi.getScrapedTerms,
+    핵심노트: wordListAPi.getKeyNote,
+    오답노트: wordListAPi.getIncorrectNote,
+    졸업노트: wordListAPi.getGraduationNote,
+  };
+
+  const { data, error, refetch, isLoading } = useQuery({
+    queryKey: [activeMenu],
+    queryFn: apiFunctions[activeMenu],
+    enabled: isLogin,
+  });
+  if (error) {
+    const apiError = error as unknown as ApiError;
+    if (apiError?.errorCode === 'NEED_LOGIN') {
+      navigate('/login');
+    }
+  }
+
+  const wordList = data
+    ? activeMenu === '스크랩'
+      ? data.dictionaryScraps
+      : activeMenu === '핵심노트'
+        ? data.keyNoteList
+        : activeMenu === '오답노트'
+          ? data.wrongAnswerNotes
+          : activeMenu === '졸업노트'
+            ? data.graduationNotes
+            : []
+    : [];
+
+  return {
+    wordList: isLogin ? wordList : [],
+    refetch: isLogin ? refetch : async () => Promise.resolve(),
+    isLoading: isLogin && isLoading,
+  };
+}
