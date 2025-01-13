bc06a36f70492d59b8e90a7d4fb2e3b657aed3fa
choycoy <passionfruits479@gmail.com>
2025-01-13 21:13:53 +0900

    feat: 공용 키워드 컴포넌트 생성

---
+++ src/components/common/Keywords.tsx
@@ -0,0 +1,19 @@
+import bookmarkIcon from '@/assets/p2/P2 에셋_2차전달/icon_bookmark_green fill22.svg';
+
+export default function Keywords({ tags }: { tags: string[] }) {
+  return (
+    <div className='flex items-center gap-x-1.5'>
+      <img src={bookmarkIcon} alt='북마크' className='h-[22px] w-[22px]' />
+      <div className='flex h-6 w-full gap-x-1 text-[10px] font-medium leading-120'>
+        {tags.map((tag, index) => (
+          <div
+            key={index}
+            className='flex items-center justify-center rounded border border-custom-gray-400 p-1.5 text-custom-gray-700 drop-shadow-md'
+          >
+            <span className='whitespace-nowrap'>#{tag}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
