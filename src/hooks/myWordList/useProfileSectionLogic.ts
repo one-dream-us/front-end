f44be1ed0fe34384cbe5f782039994b4bb49851f
choycoy <passionfruits479@gmail.com>
2025-01-14 14:08:33 +0900

    feat: 경로 연결

--- src/hooks/myWordList/useProfileSectionLogic.ts
+++ src/hooks/myWordList/useProfileSectionLogic.ts
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function useProfileSectionLogic(totalKeyNote: number) {
   const text =
     totalKeyNote >= 3 ? '준비된 퀴즈를 풀어볼까요?' : '3개의 핵심노트를 통해 퀴즈를 풀어보세요 !';
   const progressBarWidth = totalKeyNote * 80;
-  return { text, progressBarWidth };
+  const navigate = useNavigate();
+
+  return { text, progressBarWidth, navigate };
 }
