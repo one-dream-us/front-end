2af68743878393fd0c2d9f82980927060043021b
choycoy <passionfruits479@gmail.com>
2025-01-20 14:10:47 +0900

    fix: 홈 페이지 툴팁 잔상 문제 해결

--- src/components/common/Keywords.tsx
+++ src/components/common/Keywords.tsx
@@ -20,11 +20,12 @@ export default function Keywords({
             className='relative flex items-center justify-center rounded border border-custom-gray-400 p-1.5 leading-6 text-custom-gray-700 drop-shadow-md'
           >
             <span className='whitespace-nowrap'>#{tag}</span>
-            {withTooltip && showTooltip && index === 0 && (
-              <p className="absolute -left-7 -top-2.5 z-[999] -translate-y-full whitespace-nowrap rounded bg-custom-gray-dark px-2.5 py-2 text-xs text-custom-cream-light drop-shadow-xl after:absolute after:left-8 after:top-full after:border-x-[6px] after:border-t-[8px] after:border-transparent after:border-t-custom-gray-dark after:content-['']">
-                해당 머니뉴스를 통해 공부할 수 있는 단어에요.
-              </p>
-            )}
+
+            <p
+              className={`absolute -left-7 -top-2.5 z-[999] -translate-y-full whitespace-nowrap rounded bg-custom-gray-dark px-2.5 py-2 text-xs text-custom-cream-light drop-shadow-xl after:absolute after:left-8 after:top-full after:border-x-[6px] after:border-t-[8px] after:border-transparent after:border-t-custom-gray-dark after:content-[''] ${withTooltip && showTooltip && index === 0 ? 'block' : 'hidden'}`}
+            >
+              해당 머니뉴스를 통해 공부할 수 있는 단어에요.
+            </p>
           </div>
         ))}
       </div>
