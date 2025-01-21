5ed90e918921c3eaad1b239381246522bf72ef62
choycoy <passionfruits479@gmail.com>
2025-01-21 12:17:35 +0900

    fix: 나의 단어장 페이지 파라미터 변경

--- src/hooks/myWordList/useMyWordList.ts
+++ src/hooks/myWordList/useMyWordList.ts
@@ -1,35 +1,41 @@
 import { useState, useEffect } from 'react';
-// import useFirstLogin from './useFirstLogin';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { MyWordListMenuType } from '@/types/types';
+// import useFirstLogin from './useFirstLogin';
 
 export default function useMyWordList() {
   const [showTutorial, setShowTutorial] = useState(true);
   const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const initialMenu = queryParams.get('tab') || 'scrap';
+  const navigate = useNavigate();
+  //useFirstLogin
+
   const parameters: Record<string, MyWordListMenuType> = {
-    scrap: '스크랩',
-    bookmark: '핵심노트',
-    incorrect: '오답노트',
-    graduation: '졸업노트',
+    SCRAP: '스크랩',
+    KEY_NOTE: '핵심노트',
+    WRONG_ANSWER_NOTE: '오답노트',
+    GRADUATION_NOTE: '졸업노트',
   };
-  const [activeMenu, setActiveMenu] = useState<MyWordListMenuType>(
-    parameters[initialMenu] ?? '스크랩',
-  );
+
+  const queryParams = new URLSearchParams(location.search);
+  const initialMenuKey = queryParams.get('tab') || 'SCRAP';
+  const initialMenu = parameters[initialMenuKey ?? ''] || '스크랩';
+
+  const [activeMenu, setActiveMenu] = useState<MyWordListMenuType>(initialMenu);
   const [showTooltip, setShowTooltip] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const navigate = useNavigate();
-
-  // useFirstLogin(setShowTutorial);
 
   useEffect(() => {
     const currentParams = new URLSearchParams(location.search);
-    if (currentParams.get('tab') !== activeMenu) {
-      currentParams.set('tab', activeMenu);
+    const currentTab = currentParams.get('tab')?.toUpperCase();
+
+    if (parameters[currentTab ?? ''] !== activeMenu) {
+      currentParams.set(
+        'tab',
+        Object.keys(parameters).find((key) => parameters[key] === activeMenu) || 'SCRAP',
+      );
       navigate({ search: currentParams.toString() }, { replace: true });
     }
-  }, [activeMenu, location.search, navigate]);
+  }, [activeMenu, location.search, navigate, parameters]);
 
   return {
     showTutorial,
