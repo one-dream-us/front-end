a9264ad2980dab306d4c57b3763574d89bf34946
DxxxHxx <donghun1163@gmail.com>
2025-01-22 21:11:17 +0900

    refactor: 로그인 만료, 스크랩 쿼리키 수정

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -3,6 +3,7 @@ const QUERY_KEYS = {
   userInfo: ['userInfo'] as const,
   viewCount: ['myViewCount'] as const,
   newsDetail: (newsId: string) => ['news-detail', newsId] as const,
-  getScrapList: ['myScrapList'] as const,
+  // getScrapList: ['myScrapList'] as const,
+  getScrapList: ['스크랩'] as const,
 };
 export default QUERY_KEYS;
