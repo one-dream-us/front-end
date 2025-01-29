60f735a0b11c1b6576d9cd22253d9cb2840dc80d
DxxxHxx <donghun1163@gmail.com>
2025-01-29 13:49:20 +0900

    refactor: 이메일 밑줄 제거

--- src/components/profile/ProfileInfo.tsx
+++ src/components/profile/ProfileInfo.tsx
@@ -13,8 +13,8 @@ function ProfileInfo({ createdAt, email }: Pick<UserInfoData, 'email' | 'created
     >
       {info.map((item) => (
         <div key={item.title} className='h-[42px] w-[169px] text-[12px] desktop:text-[14px]'>
-          <h2 className='mb-[8px] border-none font-medium no-underline'>{item.title}</h2>
-          <span className='text-custom-gray'>{item.data}</span>
+          <h2 className='mb-[8px] font-medium'>{item.title}</h2>
+          <span className='select-none border-none text-custom-gray no-underline'>{item.data}</span>
         </div>
       ))}
     </div>
