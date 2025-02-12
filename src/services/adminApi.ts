7862624d16026a75337fdca112d3ff7fd64f13a9
DxxxHxx <donghun1163@gmail.com>
2025-02-12 18:02:09 +0900

    fix: 어드민 api 엔드포인트 수정

--- src/services/adminApi.ts
+++ src/services/adminApi.ts
@@ -4,13 +4,16 @@ import client from '@/utils/client';
 const adminApi = {
   lookUpKeyword: async (keyword: string) => (await client.get(`/dictionary/${keyword}`)).data,
   uploadScheduled: async (payload: UploadFormReqestBody, date: string) => {
-    return (await client.post(`/contents/news/scheduled/${date}`, payload)).data;
+    return (await client.post(`/back-office/contents/news/scheduled/${date}`, payload)).data;
   },
   uploadImmediately: async (payload: UploadFormReqestBody) => {
-    return (await client.post('/contents/news', payload)).data;
+    return (await client.post('/back-office/contents/news', payload)).data;
   },
   getScheduledUploadList: async (page: number, size: number = 10) => {
-    return (await client.get(`/contents/news/scheduled?page=${page}&size=${size}`)).data;
+    return (await client.get(`/back-office/contents/news/scheduled?page=${page}&size=${size}`))
+      .data;
   },
+  getUploadedList: async (page: number, size: number = 10) =>
+    (await client.get(`/back-office/contents/news?page=${page}&size=${size}`)).data,
 };
 export default adminApi;
