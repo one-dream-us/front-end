12087cea7dd75ec653292376da17f9d2f2e6c1c2
DxxxHxx <donghun1163@gmail.com>
2025-02-19 21:32:53 +0900

    feat: 학습 완료 페이지, 퀴즈 결과 페이지에 미션 확인 컴포넌트 작성

--- src/components/course/complete/CompleteButton.tsx
+++ src/components/course/complete/CompleteButton.tsx
@@ -6,7 +6,7 @@ export default function CompleteButton() {
     <button
       id='go_wordlist_news'
       onClick={() => navigate('/news-list')}
-      className='m-auto mb-[40px] mt-[54px] flex h-[48px] w-[342px] items-center justify-center rounded-[10px] bg-custom-gray-dark text-[14px] font-bold leading-[100%] text-custom-green-money transition-all duration-200 hover:bg-hover-80 hover:text-green-hover md:mt-[120px] desktop:mt-[24px] desktop:w-[440px]'
+      className='m-auto mb-[40px] mt-[54px] hidden h-[48px] w-[342px] items-center justify-center rounded-[10px] bg-custom-gray-dark text-[14px] font-bold leading-[100%] text-custom-green-money transition-all duration-200 hover:bg-hover-80 hover:text-green-hover md:mt-[120px] desktop:mt-[24px] desktop:flex desktop:w-[440px]'
     >
       학습 끝내기
     </button>
--- src/pages/quiz/QuizResultPage.tsx
+++ src/pages/quiz/QuizResultPage.tsx
@@ -1,21 +1,17 @@
 import QuizResultItem from '@/components/quiz/quizResult/QuizResultItem';
-import BottomSheet from '@/components/quiz/common/BottomSheet';
-import BottomSheetImg from '@/assets/p2/icon_quiz.png';
 import iconGrad from '@/assets/p2/icon_grad.png';
 import { NORMAL_QUIZ_RESULT_KEY } from '@/constants/constants';
 import { IQuizResult } from '@/types/interface';
-import { useNavigate } from 'react-router-dom';
 import { createTitle } from '@/utils/quiz/quizHandlers';
 import NormalQuizResultCard from '@/components/quiz/quizResult/NormalQuizResultCard';
 import { useUserInfoQuery } from '@/hooks/auth/useUserInfoQuery';
+import MissionCheckComponent from '@/components/common/MissionCheckComponent';
 
 export default function QuizResultPage() {
   const { accuracyRate, graduationCnt, resultDetails, totalWrong }: IQuizResult = JSON.parse(
     localStorage.getItem(NORMAL_QUIZ_RESULT_KEY) as string,
   );
   console.log(resultDetails);
-
-  const navigate = useNavigate();
 
   /**결과에 졸업 단어가 하나라도 있는지 확인 */
   const isGraduate = resultDetails
@@ -24,11 +20,6 @@ export default function QuizResultPage() {
 
   const { data } = useUserInfoQuery(!isGraduate);
   const title = createTitle(accuracyRate, isGraduate, !isGraduate ? data?.name : undefined);
-
-  const handleBottomSheetClick = () =>
-    accuracyRate === 100
-      ? navigate('/my-word-list?tab=스크랩')
-      : navigate('/my-word-list?tab=오답노트');
 
   return (
     <div className='m-auto'>
@@ -54,20 +45,14 @@ export default function QuizResultPage() {
 
       <div className='left-0 h-auto w-full desktop:absolute desktop:bg-quiz-bg desktop:pt-[24px]'>
         {/* questions */}
-        <ul className='m-auto mb-[200px] max-w-[353px] desktop:grid desktop:max-w-[812px] desktop:grid-cols-2 desktop:gap-x-[20px] desktop:gap-y-[12px]'>
+        <ul className='m-auto max-w-[353px] pb-[350px] desktop:grid desktop:max-w-[812px] desktop:grid-cols-2 desktop:gap-x-[20px] desktop:gap-y-[12px]'>
           {resultDetails.map((item, index) => (
             <NormalQuizResultCard key={`${item.term}_${index}`} {...item} />
           ))}
         </ul>
       </div>
 
-      <BottomSheet
-        titleText='오늘의 퀴즈 완료!'
-        imgSrc={BottomSheetImg}
-        buttonTextColor='text-custom-green-money'
-        handleButtonClick={handleBottomSheetClick}
-        buttonText={accuracyRate === 100 ? '외운 단어 확인하기' : '복습하러가기'}
-      />
+      <MissionCheckComponent clear={true} />
     </div>
   );
 }
