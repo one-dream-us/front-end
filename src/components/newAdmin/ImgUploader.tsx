15ce9a74e8b7dfa012dbb9b478ad5dcf9a1bc54e
DxxxHxx <donghun1163@gmail.com>
2025-02-22 21:33:55 +0900

    feat: 이미지 업로더 컴포넌트 작성

---
+++ src/components/newAdmin/ImgUploader.tsx
@@ -0,0 +1,43 @@
+import { AdminImgUploadeerProps } from '@/types/interface';
+
+export default function ImgUploader({ handleImageChange, imagePreview }: AdminImgUploadeerProps) {
+  return (
+    <div className='rounded-md border-2 border-dashed p-4 text-center'>
+      <input
+        type='file'
+        id='image'
+        accept='image/*'
+        className='hidden'
+        onChange={handleImageChange}
+      />
+      <label htmlFor='image' className='flex cursor-pointer flex-col items-center justify-center'>
+        {imagePreview ? (
+          <img
+            src={imagePreview}
+            alt='Preview'
+            className='mb-2 max-h-48 rounded-md object-contain'
+          />
+        ) : (
+          <>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='mb-2 h-8 w-8 text-gray-400'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15'
+              />
+            </svg>
+
+            <span className='text-sm text-gray-500'>클릭하여 이미지를 업로드하세요</span>
+          </>
+        )}
+      </label>
+    </div>
+  );
+}
