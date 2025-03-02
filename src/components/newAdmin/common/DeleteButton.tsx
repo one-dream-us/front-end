25ab5db8e33a2411f47baf6e35d1c72e45f2c07d
DxxxHxx <donghun1163@gmail.com>
2025-03-02 18:14:30 +0900

    feat: 예약 컨텐츠 삭제 연동

--- src/components/newAdmin/common/DeleteButton.tsx
+++ src/components/newAdmin/common/DeleteButton.tsx
@@ -1,11 +1,18 @@
 import adminApi from '@/services/adminApi';
+import { useLocation } from 'react-router-dom';
 
 export default function DeleteButton({ id }: { id: number }) {
+  const { pathname } = useLocation();
+  const isScheduledPage = pathname.includes('content');
   const handleRemoveContents = async () => {
     if (!confirm('컨텐츠를 삭제합니다.')) return;
 
     try {
-      await adminApi.deleteDraftContents(+id);
+      if (isScheduledPage) {
+        await adminApi.deleteScheduledContents(id);
+      } else {
+        await adminApi.deleteDraftContents(+id);
+      }
       location.pathname = '/admin/home';
     } catch (e) {
       console.log(e);
