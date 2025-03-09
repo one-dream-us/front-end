ded8d7e588457dcb9ec5eebaca5553c5eb9875a8
DxxxHxx <donghun1163@gmail.com>
2025-03-09 13:01:45 +0900

    북마크 요청 mocking

--- src/components/course/complete/CardContainer.tsx
+++ src/components/course/complete/CardContainer.tsx
@@ -1,8 +1,28 @@
 import useNewsDetail from '@/hooks/newDetail/useNewsDetail';
 import CompleteWordCard from './CompleteWordCard';
+// import { useEffect } from 'react';
+// import { useMutation } from '@tanstack/react-query';
+// import axios from 'axios';
+
+// interface PayloadTypes {
+//   dictionaryIds: {
+//     id: number;
+//   }[];
+// }
 
 export default function CardContainer() {
   const { news, isLoading } = useNewsDetail((data) => data.descriptions);
+
+  // const { mutate: addHistory } = useMutation({
+  //   mutationFn: async (payload: PayloadTypes) => (await axios.post('/history', payload)).data,
+  // });
+
+  // useEffect(() => {
+  //   if (!isLoading) {
+  //     const res = news?.map(({ dictionaryId }) => ({ id: dictionaryId }));
+  //     addHistory({ dictionaryIds: res! });
+  //   }
+  // }, [isLoading]);
 
   return (
     <>
