33e7241e00fe9a677c05ef45f591d8414ffd0fe0
choycoy <passionfruits479@gmail.com>
2025-03-17 07:44:26 -0930

    feat: 나의 단어장 히스토리 3개 이하시 문구 추가

--- src/components/myWordList/WordList.tsx
+++ src/components/myWordList/WordList.tsx
@@ -2,7 +2,8 @@ import { WordListProps } from '@/types/interface';
 import useGetWordListData from '@/hooks/myWordList/api/useGetWordListData';
 import EmptyWordState from './Words/EmptyWordState';
 import WordListContent from './Words/WordListContent';
-
+import { Link } from 'react-router-dom';
+import arrowRightIcon from '@/assets/p2/arrow_right.png';
 export default function WordList({
   activeMenu,
   setShowTooltip,
@@ -17,11 +18,18 @@ export default function WordList({
 
   return (
     <div className='relative box-content w-[343px] px-4 md:w-[353px] md:px-0 desktop:w-[812px]'>
-      <p
-        className={`${activeMenu === '북마크' && wordNum < 3 ? '' : 'mb-[23px]'} text-xs font-medium text-custom-gray-700`}
-      >
+      <p className='mb-6 text-xs font-medium text-custom-gray-700'>
         단어 수<span className='ml-2 font-bold'>{wordNum}</span>
       </p>
+      {activeMenu === '히스토리' && wordNum < 3 && wordNum > 0 ? (
+        <div className='my-5 flex flex-col items-center gap-y-2 text-center text-sm leading-170 text-custom-gray-500'>
+          <p>퀴즈를 풀기 위해서는 히스토리가 더 필요해요.</p>
+          <Link to='/news-list' className='flex items-center'>
+            <span>머니뉴스 보러 가기</span>
+            <img src={arrowRightIcon} alt='오른쪽 화살표' className='h-5 w-5' />
+          </Link>
+        </div>
+      ) : null}
       {activeMenu === '북마크' && wordNum < 3 && wordNum > 0 ? (
         <p className='my-5 text-center text-sm leading-170 text-custom-gray-500'>
           퀴즈를 풀기 위해서는 북마크가 더 필요해요.
