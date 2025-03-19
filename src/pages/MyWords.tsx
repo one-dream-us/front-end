3ebcf404dd324d3193e66ab7e530ae09e90a47ce
choycoy <passionfruits479@gmail.com>
2025-03-19 13:33:36 +0900

    fix: 나의 단어장 퀴즈 인트로 제거 적용

--- src/pages/MyWords.tsx
+++
@@ -1,9 +0,0 @@
-import useIsFirstQuiz from '@/hooks/myWordList/api/useIsFirstQuiz';
-import MyWordList from './MyWordList';
-
-export default function MyWords() {
-  const { isCheckFirstLoading } = useIsFirstQuiz();
-  if (isCheckFirstLoading) return null;
-
-  return <MyWordList />;
-}
