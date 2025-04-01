ef3425ae1d50d9a47a9a5bfd62cd14a0ab2ad105
choycoy <passionfruits479@gmail.com>
2025-04-01 17:05:01 +0900

    refactor: styles.css 제거 후 index.css에 통합

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '@/assets/fonts/font.css';
-import '@/styles/styles.css';
 
 const enableMocking = async () => {
   // if (process.env.NODE_ENV !== 'development') {
