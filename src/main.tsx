919848f2223dd038bfaffbae40213665d579ae5a
choycoy <passionfruits479@gmail.com>
2025-04-01 20:22:24 +0900

    refactor: gtm코드에 partytown 적용

--- src/main.tsx
+++ src/main.tsx
@@ -22,3 +22,28 @@ const enableMocking = async () => {
     console.log(e);
   }
 })();
+
+window.GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+
+const insertGTM = () => {
+  const gtmId = import.meta.env.VITE_GTM_ID;
+
+  if (gtmId) {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', '${gtmId}');
+    `;
+    document.head.appendChild(script);
+  }
+};
+
+window.addEventListener('DOMContentLoaded', insertGTM);
